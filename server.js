61debddc160f6fd11928a1ef277c184586890bcf
khoshow <khoshow.official@gmail.com>
2021-10-13 01:52:02 +0530

    edit cors

--- server.js
+++ server.js
@@ -21,13 +21,11 @@ const replyRoutes = require('./routes/replies')
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-
-app.use(cors({ origin: `${process.env.CLIENT_URL_PRODUCTION}` }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'https://nagamei.com');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // db
 let productionOrDevelopment;
